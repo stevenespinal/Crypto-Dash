457b74e30fa6cceb54a1df79d5ad76fe9868ff54
Steven Espinal <stevenjesusespinal@gmail.com>
2019-05-02 17:54:54 -0400

    Added Styled Components

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Welcome from './WelcomeMessage';
+// import styled, {css} from 'styled-components'
 import './App.css';
 
-function Index() {
-  return (
-    <div>
-      Hello World
-    </div>
-  );
+
+// const MyButton = styled.button`
+// color: green;
+// ${props => props.primary && css`
+// color: palevioletred;
+// `}
+// `;
+// const TomatoButton = styled(MyButton)`
+// color: tomato;
+// border-color: tomato;
+// `;
+
+class Index extends Component {
+  render() {
+    return (
+        <div>
+          <Welcome/>
+        </div>
+    );
+  }
 }
 
 export default Index;
