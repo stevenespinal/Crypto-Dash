07a30105e6c21a14d12b2fd43b64ed0cd00d04b2
Steven Espinal <stevenjesusespinal@gmail.com>
2019-05-02 19:37:39 -0400

    Add AppProvider and Connect it to AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -1,27 +1,19 @@
 import React, {Component} from 'react';
 import Welcome from './WelcomeMessage';
-// import styled, {css} from 'styled-components'
 import './App.css';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
+import AppProvider from './AppProvider';
 
-// const MyButton = styled.button`
-// color: green;
-// ${props => props.primary && css`
-// color: palevioletred;
-// `}
-// `;
-// const TomatoButton = styled(MyButton)`
-// color: tomato;
-// border-color: tomato;
-// `;
 
 class Index extends Component {
   render() {
     return (
         <AppLayout>
-          <AppBar/>
-          <Welcome/>
+          <AppProvider>
+            <AppBar/>
+            <Welcome/>
+          </AppProvider>
         </AppLayout>
     );
   }
