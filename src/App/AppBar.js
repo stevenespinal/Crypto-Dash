b4ce6c6ef57d2aa526344513e916d94330396dbf
Steven Espinal <stevenjesusespinal@gmail.com>
2019-05-07 11:02:20 -0400

    Fixed dashboard firstVisit bug

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -17,6 +17,9 @@ cursor: pointer;
 ${props => props.active && css`
 text-shadow: 0px 0px 60px #03ff03;
 `}
+${props => props.hidden && css`
+display: none;
+`}
 `;
 
 
@@ -28,10 +31,11 @@ function toProperCase(lower) {
 function ControlButton({name}) {
   return (
       <AppContext.Consumer>
-        {({page, setPage}) => (
+        {({firstVisit, page, setPage}) => (
             <ControlButtonElem
                 active={page === name}
                 onClick={() => setPage(name)}
+                hidden={firstVisit && name === 'dashboard'}
             >
               {toProperCase(name)}
             </ControlButtonElem>
